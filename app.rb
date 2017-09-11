97bf6ce3a509b9d826a8f50b5e5286bc1f739241
Erik Zakrzewski and Rafael Furry <erik+rafael@>
2017-09-11 14:04:16 -0700

    finish the triangle app

--- app.rb
+++ app.rb
@@ -1,6 +1,17 @@
 require('sinatra')
 require('sinatra/reloader')
 
+require ('./lib/triangles') # ./lib not .lib
+
 get('/triangle') do
   erb(:input)
+end
+
+get('/output') do
+  @side_1 = params.fetch("side_1")
+  @side_2 = params.fetch("side_2")
+  @side_3 = params.fetch("side_3")
+  tri = Triangles.new(@side_1, @side_2, @side_3)
+  @answer = tri.tri_type
+  erb(:output)
 end
